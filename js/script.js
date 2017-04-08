8f2f1fdf8480fa3342744409fd6b86a4c62e8f2b
keita <ketainfo@gmail.com>
2017-04-08 23:36:17 +0900

    jQuery カウントアップ用

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
  * 実行手順1:初期値設定
  * $(セレクタ).countUp({
  *  speed:実行スピード,
- *  countMax:最大値
+ *  countMax:最大値,
+ *  fadeIn:true | false
  * });
  *
  * 実行手順2:カウントアップ実行
@@ -12,35 +13,42 @@
 (function($) {
 	var settings = { //カウントアップ実行初期値
 			speed: 1000,
-			countMax: 100
+			countMax: 100,
+			fadeIn: false
 		},
+
 		methods = {
-			init: function(opt) {//初期設定
-				return $.extend(settings, opt);
+			init: function(opt) { //初期設定
+				return this.each(function() {
+					settings = $.extend({}, settings, opt);
+					$(this).on('click', methods.countStop);
+				});
 			},
-			exe: function() {//カウントアップ実行
-				this.animate({ i: settings.countVal }, {
+			exe: function() { //カウントアップ実行
+				if (settings.fadeIn) { this.css({ opacity: 0 }); }
+				return this.animate({ opacity: 1, now: settings.countMax }, {
 					duration: settings.speed,
-					step: function(a) {
-						$(this).text(a | 0);
+					step: function(now) {
+						$(this).text(now | 0);
 					}
 				});
-			}
+			},
 		}
 
-	$.fn.countUp = function(name) {
 
-		//exe
-		if (methods[name]) {
-			console.log(Array.prototype.slice.call(arguments));
+	$.fn.countUp = function(name) {
+		if (methods[name]) { //exe
 			return methods[name].apply(this, Array.prototype.slice.call(arguments, 1));
 		} else { // init
 			return methods.init.apply(this, arguments);
 		}
-
-
 	};
 })(jQuery);
 
-$('span').countUp({ speed: 10000, countVal: 500 });//初期値設定
-$('span').countUp('exe');//実行
+
+$('.c1').countUp({ speed: 2000, countMax: 500, fadeIn: true }); //初期値設定
+$('.c1').countUp('exe'); //実行
+
+
+$('.c2').countUp({ speed: 1000, countMax: 1000 }); //初期値設定
+$('.c2').countUp('exe'); //実行
