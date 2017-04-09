8d8ec21e50618b8f37ce70c287004448557e2355
keita <ketainfo@gmail.com>
2017-04-09 09:00:52 +0900

    htmlファイル追加,jsファイル修正

--- js/countup.js
+++ js/countup.js
@@ -4,7 +4,7 @@
  * $(セレクタ).countUp({
  *  speed:実行スピード,
  *  countMax:最大値,
- *  fadeIn:true | false
+
  * });
  *
  * 実行手順2:カウントアップ実行
@@ -16,25 +16,22 @@
 			countMax: 100,
 			fadeIn: false
 		},
-
 		methods = {
 			init: function(opt) { //初期設定
 				return this.each(function() {
 					settings = $.extend({}, settings, opt);
-					$(this).on('click', methods.countStop);
 				});
 			},
 			exe: function() { //カウントアップ実行
 				if (settings.fadeIn) { this.css({ opacity: 0 }); }
 				return this.animate({ opacity: 1, now: settings.countMax }, {
 					duration: settings.speed,
-					step: function(now) {
+					step: function(now, tween) {
 						$(this).text(now | 0);
 					}
 				});
-			},
+			}
 		}
-
 
 	$.fn.countUp = function(name) {
 		if (methods[name]) { //exe
@@ -44,11 +41,3 @@
 		}
 	};
 })(jQuery);
-
-
-$('.c1').countUp({ speed: 2000, countMax: 500, fadeIn: true }); //初期値設定
-$('.c1').countUp('exe'); //実行
-
-
-$('.c2').countUp({ speed: 1000, countMax: 1000 }); //初期値設定
-$('.c2').countUp('exe'); //実行
